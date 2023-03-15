92c048d86512e9a417cb32c768421dc4eed69822
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-03-14 22:40:25 -0700

    Organize file structure

--- blogList/.eslintrc.js
+++ blogList/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
 	rules: {
 		'no-console': 0,
 		'no-underscore-dangle': 0,
-		"no-param-reassign": 0
+		'no-param-reassign': 0,
+		'prefer-destructuring': 0,
 	},
 };
--- blogList/app.js
+++ blogList/app.js
@@ -1,49 +1,38 @@
-// const http = require('http');
-require('dotenv').config();
+const config = require('./utils/config');
 const express = require('express');
 
 const app = express();
 const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 
-const blogSchema = new mongoose.Schema({
-	title: String,
-	author: String,
-	url: String,
-	likes: Number,
-});
+mongoose.set('strictQuery', false);
 
-blogSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
-});
+logger.info('connecting to', config.MONGODB_URI);
 
-const Blog = mongoose.model('Blog', blogSchema);
-
-mongoose.connect(process.env.MONGODB_URI);
+mongoose
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB');
+	})
+	.catch((error) => {
+		logger.error('error connecting to MongoDB:', error.message);
+	});
 
 app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.get('/api/blogs', (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs);
-	});
-});
+app.use('/api/blogs', blogsRouter);
 
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-	blog.save().then((result) => {
-		response.status(201).json(result);
-	});
-});
+// app.listen(config.PORT, () => {
+// 	logger.info(`bacon`);
+// });
 
-const port = process.env.PORT;
-app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
-});
+module.exports = app;
